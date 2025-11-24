01d6f651323f2bf802b7e9a5a76b57db01cc3d4b
ssannejohansson <sannej0hansson@hotmail.com>
2025-11-24 10:30:09 +0100

    Set up webhooks

--- MS4/settings.py
+++ MS4/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-d=tyo9h2mco%0o7#7$@1co2c_41pyu-&gnyeg@*=i_mj20ft6=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
@@ -183,6 +186,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
