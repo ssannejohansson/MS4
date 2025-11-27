65493ed1fb5ea16fee188a545f4bacb1a2fd952a
ssannejohansson <sannej0hansson@hotmail.com>
2025-11-27 16:19:13 +0100

    Add profile form, views and update template

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,9 @@
+const countrySelect = document.getElementById('id_default_country');
+
+function updateColor() {
+    const value = countrySelect.value;
+    countrySelect.style.color = value ? '#232120' : '#81726c';
+}
+
+updateColor();
+countrySelect.addEventListener('change', updateColor);
