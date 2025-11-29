827b1f1b1ab6ddb464cd1b43c0e68a7371b57a7f
ssannejohansson <sannej0hansson@hotmail.com>
2025-11-29 09:17:29 +0100

    Add add_product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import ProductVariant, Category
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        model = ProductVariant
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
@@ -15,4 +15,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget_attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- products/models.py
+++ products/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
     sku = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
