7101354233c23b67eedd4a73da5776eaead70953
ssannejohansson <sannej0hansson@hotmail.com>
2025-12-02 12:42:09 +0100

    Move custom_storages.py to test if it fixes the error

--- test_s3.py
+++ test_s3.py
@@ -1,22 +1,20 @@
-# test_s3.py
 import os
 
-# Make sure env.py runs first
-if os.path.isfile('env.py'):
+# Load env.py so the variables exist
+if os.path.isfile("env.py"):
     import env
 
 import boto3
 
-# Now these variables are guaranteed to exist
+bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+print("Bucket name:", bucket_name)
+
 s3 = boto3.client(
     "s3",
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
     region_name="eu-north-1"
 )
-
-bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-print("Bucket name:", bucket_name)  # debug line to make sure it's set
 
 response = s3.list_objects_v2(Bucket=bucket_name)
 print(response)
