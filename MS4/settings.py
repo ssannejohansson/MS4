a58971842511cd6a390cfbac03a7ebb43d7aaf93
ssannejohansson <sannej0hansson@hotmail.com>
2025-12-04 11:58:06 +0100

    Test updated settings for AWS and storages

--- MS4/settings.py
+++ MS4/settings.py
@@ -191,6 +191,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.environ.get('USE_AWS') == 'true':
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_OBJECT_PARAMETERS = {
+        "ACL": "public-read"
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'the-poster-vault'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -198,15 +203,21 @@ if os.environ.get('USE_AWS') == 'true':
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+    # Django 5+ storage settings
+STORAGES = {
+    "default": {
+        "BACKEND": "custom_storages.MediaStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "custom_storages.StaticStorage",
+    },
+}
 
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
--- custom_storages.py
+++ custom_storages.py
@@ -3,11 +3,20 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'
+    """Storage for static files."""
+    location = getattr(settings, "STATICFILES_LOCATION", "static")
+    file_overwrite = True
+    default_acl = None  # Let bucket policy control ACL
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
-    default_acl = 'public-read'
+    """Storage for user-uploaded media."""
+    location = getattr(settings, "MEDIAFILES_LOCATION", "media")
     file_overwrite = False
+    default_acl = None  # Let bucket policy control ACL
+
+
+def get_default_settings(self):
+    settings = super().get_default_settings()
+    settings["default_acl"] = "public-read"
+    return settings
