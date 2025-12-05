021cabb59790013d924a41aaa61e20c79d9f336c
ssannejohansson <sannej0hansson@hotmail.com>
2025-12-05 09:58:08 +0100

    Change default from email to make emails send correctly

--- MS4/settings.py
+++ MS4/settings.py
@@ -224,7 +224,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'thepostervault@example.com'
+    DEFAULT_FROM_EMAIL = 'sannejohansson7@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
