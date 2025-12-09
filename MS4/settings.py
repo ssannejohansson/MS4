d843cc99d0fd0debc25db2f68acb4e39abfb5107
ssannejohansson <sannej0hansson@hotmail.com>
2025-12-09 10:43:03 +0100

    Change default from email

--- MS4/settings.py
+++ MS4/settings.py
@@ -228,7 +228,7 @@ CONTACT_RECEIVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'sannejohansson7@gmail.com'
+    DEFAULT_FROM_EMAIL = 'The Poster Vault <no-reply@thepostervault.com>'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -236,7 +236,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'The Poster Vault <no-reply@thepostervault.com>'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
